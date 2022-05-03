0a6459018a953e9ec9b387ce51fed9b7bbc565ab
Lucasshaw12 <shaw51@hotmail.co.uk>
2022-05-03 16:42:50 +0100

    Completed project. Access a GoogleSheets form, trim the empty cells and only display the email addresses

---
+++ Google Sheets/downloadGoogleSheetsForm.py
@@ -0,0 +1,31 @@
+#! python3
+# Download Google Sheets form data and display all the email addresses
+
+
+import ezsheets
+# Access the spreadsheet
+ss = ezsheets.Spreadsheet('1ynkLoQAxZDn3kkKu5PK4rpJ5UgAK5Cpzg9Z8o6a1bgY')
+sheet = ss[0]
+columns = sheet.getColumn(2)
+rows = sheet.getRows()
+
+# Trim the list. Then use the list to count the number of responses
+numOfResponses = []
+for col in columns:
+	if col != '':
+		numOfResponses.append(col)
+
+numOfQuestions = []
+for row in rows[1:]: # Ignore the first row
+	if row != '':
+		numOfQuestions.append(row)
+
+rowLen = len(numOfResponses) # Set the amount of rows respective to the responses received
+sheet.rowCount = rowLen
+
+colLen = len(numOfQuestions) # Set the amount of columns respective to the questions asked
+sheet.columnCount = colLen
+
+emailAddresses = sheet.getColumn(3) # Get the column that contains the email addresses
+for i, value in enumerate(emailAddresses[1:]): # Again, ignore the first row
+	print(i, value)
